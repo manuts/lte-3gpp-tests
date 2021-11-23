7be0ac293515e92da41c17bd28ea960a2ef717bb
Manu Thottathil Sreedharan <mthottathilsreed@apple.com>
2021-11-23 22:41:05 +0100

    restructure test proj

---
+++ src/tests.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : tests.cpp
+// Author      : Manu T. Sreedharan
+// Version     :
+// Copyright   : Copyright (C) 2021 Manu T. Sreedharan
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int todo() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
